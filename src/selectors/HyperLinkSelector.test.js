7141b684e16e24f36dc3eed628ba0368009b8863
dreamer <wildetangeant@gmail.com>
2019-06-20 13:54:52 -0700

    post selector test passed

---
+++ src/selectors/HyperLinkSelector.test.js
@@ -0,0 +1,20 @@
+import { getHyperLinksList } from './HyperLinksSelector';
+
+
+describe('post selectors', () => {
+  it('gets all posts', () => {
+    const state = {
+      hyperlinks: {
+        hyperlinkList: [
+          { url: 'one' },
+          { url: 'two' }
+        ]
+      } };
+
+    expect(getHyperLinksList(state)).toEqual([
+      { url: 'one' },
+      { url: 'two' },
+    ]);
+  });
+})
+;
