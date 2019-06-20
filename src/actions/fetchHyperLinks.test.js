e7a717662bbb254c38c6ff78bbf7bdf3622099f8
dreamer <wildetangeant@gmail.com>
2019-06-20 12:03:57 -0700

    action test for GET passed+mocks written

---
+++ src/actions/fetchHyperLinks.test.js
@@ -0,0 +1,23 @@
+import {
+  fetchHyperLinks,
+  FETCH_HYPERLINKS,
+  FETCH_HYPERLINKS_PENDING,
+} from './fetchHyperLinksaction';
+
+jest.mock('../services/LinksApi.js', () => ({
+  getHyperLinks() {
+    return Promise.resolve([]);
+  }
+}));
+
+describe('hyperlink actions', () => {
+  it('fetches a list of hyperlinks and dispatches an action', () => {
+    expect(fetchHyperLinks()).toEqual({
+      type: FETCH_HYPERLINKS,
+      pendingType: FETCH_HYPERLINKS_PENDING,
+      payload: expect.any(Promise),
+      fulfilledType: 'FETCH_HYPERLINKS_FULFILLED',
+      rejectedType: 'FETCH_HYPERLINKS_REJECTED'
+    });
+  });
+});
