e911eeaa7c70cadd1ca031c7d7ef204e9445fb8b
dreamer <wildetangeant@gmail.com>
2019-06-20 11:09:03 -0700

    Hyperlinks action written

---
+++ src/actions/fetchHyperLinks.js
@@ -0,0 +1,11 @@
+import { createAction } from 'promise-middleware-redux';
+import getHyperLinks  from '../services/LinksApi';
+
+export const [
+  fetchHyperLinks,
+  FETCH_HYPERLINKS,
+  FETCH_HYPERLINKS_PENDING,
+  _,
+  FETCH_HYPERLINKS_ERROR
+] = createAction('FETCH_HYPERLINKS', getHyperLinks);
+
