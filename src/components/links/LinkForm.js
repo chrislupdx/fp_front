b76a391f28a3ecc52db674b41a30ff33a1952be0
dreamer <wildetangeant@gmail.com>
2019-06-21 10:41:12 -0700

    Link form+container built

---
+++ src/components/links/LinkForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function LinkForm({ onSubmit, onChange, url, submitText }) {
+  return (
+    <form onSubmit={onSubmit}>
+      <input name="url" value={url} onChange={onChange} />
+      <button>{submitText}</button>
+    </form>
+  );
+}
+
+LinkForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  url: PropTypes.string.isRequired,
+  submitText: PropTypes.string
+};
+
+LinkForm.defaultProps = {
+  submitText: 'Compress Url'
+};
+
+export default LinkForm;
---
+++ src/containers/CreateLinkContainer.js
@@ -0,0 +1,50 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { postHyperLink } from '../actions/postHyperLinksAction';
+import LinkForm from '../components/links/LinkForm';
+
+class CreateLinkContainer extends PureComponent {
+    static propTypes = {
+      CreateLink: PropTypes.func.isRequired
+    }
+
+    state = {
+      url: ''
+    }
+
+    handleSubmit = event => {
+      event.preventDefault();
+      const { url } = this.state;
+      this.props.CreateLink({ url });
+      this.setState({ url: '' });
+    }
+
+    handleChange = ({ target }) => {
+      this.setState({ [target.name]: target.value });
+    }
+
+    render() {
+      const { url } = this.state;
+      return (
+        <LinkForm
+          onSubmit={this.handleSubmit}
+          onChange={this.handleChange}
+          url={url}
+          submitText="Compress Url"
+        />
+      );
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+  CreateLink(url) {
+    dispatch(postHyperLink(url));
+  }
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CreateLinkContainer);
+
