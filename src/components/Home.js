349b995caad7a90d21d1c8b4bbb5ecf4d77a1ccc
dreamer <wildetangeant@gmail.com>
2019-06-21 10:41:19 -0700

    added form container to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HyperLinkListContainer from '../containers/HyperLinkListContainer';
+import CreateLinkContainer from '../containers/CreateLinkContainer';
 
 export default function Home() {
   return (
     <>
-        <HyperLinkListContainer />
+      <CreateLinkContainer />
+      <HyperLinkListContainer />
     </>
   );
 }
