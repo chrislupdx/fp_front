3509b8ef5dde8872f6e2f454d26134c841c5d8c9
dreamer <wildetangeant@gmail.com>
2019-06-21 09:55:56 -0700

    Built a home container, put the list container in it, routed home in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import HyperLinkListContainer from '../containers/HyperLinkListContainer';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from 'react-router-dom';
+import Home from '../components/Home';
+
 
 export default function App() {
   return (
     <Router>
       <Link to="/">Home</Link>
       <Switch>
-        <Route exact path ="/" component ={HyperLinkListContainer} />
+        <Route exact path ="/" component ={Home} />
       </Switch>
     </Router>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HyperLinkListContainer from '../containers/HyperLinkListContainer';
+
+export default function Home() {
+  return (
+    <>
+        <HyperLinkListContainer />
+    </>
+  );
+}
