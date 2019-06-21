fc97b458974cfb93c722310873698bc1a0ad3c15
dreamer <wildetangeant@gmail.com>
2019-06-21 12:57:37 -0700

    by id service written

--- src/services/LinksApi.js
+++ src/services/LinksApi.js
@@ -2,3 +2,4 @@ import { post, get } from './request';
 
 export const getHyperLinks = () => get('api/v1/hyperlinks');
 export const createHyperLink = link => post('api/v1/hyperlinks', link);
+export const getLinkById = id => get(`api/v1/hyperlinks/${id}`);
