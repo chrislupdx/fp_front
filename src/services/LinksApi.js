e8c19ba863d8eea56e95d475c1475b89d08962ad
dreamer <wildetangeant@gmail.com>
2019-06-20 18:11:01 -0700

    Post link service built

--- src/services/LinksApi.js
+++ src/services/LinksApi.js
@@ -1,3 +1,4 @@
-import { get } from './request';
+import { post, get } from './request';
 
 export const getHyperLinks = () => get('api/v1/hyperlinks');
+export const createHyperLink = link => post('api/v1/hyperlinks', link);
