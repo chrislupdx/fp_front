d039b454755fe87cea8c86794ea87f5c437e3795
dreamer <wildetangeant@gmail.com>
2019-06-21 21:53:53 -0700

    index reducer updated, link by id reducer written

---
+++ src/reducers/LinksByIdReducer.js
@@ -0,0 +1,21 @@
+import {
+  FETCH_HYPERLINKS_BY_ID,
+  FETCH_HYPERLINKS_BY_ID_PENDING
+} from '../actions/fetchbyIdAction';
+
+const initialState = {
+  hyperlink: null,
+  loading: null,
+  error: null
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case FETCH_HYPERLINKS_BY_ID:
+      return { ...state, hyperlink: action.payload, loading: false };
+    case FETCH_HYPERLINKS_BY_ID_PENDING:
+      return { ...state, loading: true };
+    default:
+      return state;
+  }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import hyperlinks from './HyperLinksReducer';
+import hyperlinkbyid from './LinksByIdReducer';
 
 export default combineReducers({
-  hyperlinks
+  hyperlinks,
+  hyperlinkbyid
 });
