319f1135ff510e94f62ed8bc33a49039e0c7b536
dreamer <wildetangeant@gmail.com>
2019-06-27 16:17:09 -0700

    patchLinkById service written

--- src/services/LinksApi.js
+++ src/services/LinksApi.js
@@ -1,6 +1,7 @@
-import { post, get, del } from './request';
+import { post, get, del, patch } from './request';
 
 export const getHyperLinks = () => get('api/v1/hyperlinks');
 export const createHyperLink = link => post('api/v1/hyperlinks', link);
 export const getLinkById = id => get(`api/v1/hyperlinks/${id}`);
 export const deleteLinkById = id => del(`api/v1/hyperlinks/${id}`);
+export const patchLinkById = (id, body) => patch(`api/v1/hyperlinks/${id}`, body);
